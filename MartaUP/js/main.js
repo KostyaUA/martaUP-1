f683c6d7fad441d1f5718cbade3d8b5df43defba
David Kasumov <davidkasumovFrontend@gmail.com>
2022-05-23 20:36:57 +0300

    initial commit

---
+++ MartaUP/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  })
+})
